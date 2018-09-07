80faee3388e235212e5b566894014c6de86766c3
Magesh <tkmagesh77@gmail.com>
2018-09-07 14:32:53 +0530

    using pipe method of readable stream

--- 04-printFile/printFileStream.js
+++ 04-printFile/printFileStream.js
@@ -2,12 +2,24 @@ var fs = require('fs');
 
 var stream = fs.createReadStream('./sample.txt', {encoding : 'utf8'});
 
-stream.on('data', function(chunk){
+/*stream.on('data', function(chunk){
 	console.log(chunk);
 });
 
 stream.on('end', function(){
 	console.log('Thats all folks!');
+})*/
+
+stream.pipe(process.stdout);
+
+var readCount = 0;
+stream.on('data', function(chunk){
+	++readCount;
+});
+
+stream.on('end', function(){
+	console.log('Thats all folks!');
+	console.log('Read operation completed with ' + readCount + ' reads');
 })
 
 stream.on('error', function(err){
