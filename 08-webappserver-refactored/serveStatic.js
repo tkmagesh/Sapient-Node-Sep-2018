d6ccfead63fffb28fa620c1cd325c3e1b2d36d2d
Magesh <tkmagesh77@gmail.com>
2018-09-08 11:48:05 +0530

    refactoring the data parsing logic to dataParser

--- 08-webappserver-refactored/serveStatic.js
+++ 08-webappserver-refactored/serveStatic.js
@@ -7,21 +7,23 @@ function isStatic(resource){
 	return staticExtns.indexOf(path.extname(resource)) >= 0;
 }
 
-module.exports = function(req, res, next){
-	if (isStatic(req.urlObj.pathname)){
-		var resourceName = path.join(__dirname, req.urlObj.pathname);
-		if (!fs.existsSync(resourceName)){
-			res.statusCode=404;
-			res.end();
-			return;
-		}
-		var stream = fs.createReadStream(resourceName);
-		stream.pipe(res);
-		stream.on('end', function(){
-			res.end();
+module.exports = function(staticPath){
+	return function(req, res, next){
+		if (isStatic(req.urlObj.pathname)){
+			var resourceName = path.join(staticPath, req.urlObj.pathname);
+			if (!fs.existsSync(resourceName)){
+				res.statusCode=404;
+				res.end();
+				return;
+			}
+			var stream = fs.createReadStream(resourceName);
+			stream.pipe(res);
+			stream.on('end', function(){
+				res.end();
+				next();
+			});
+		} else {
 			next();
-		});
-	} else {
-		next();
-	}
+		}
+	};
 }
