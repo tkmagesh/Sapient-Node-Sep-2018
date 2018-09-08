98089547b8995838b7de4157c8ac854c001e3819
Magesh <tkmagesh77@gmail.com>
2018-09-08 09:28:48 +0530

    refactoring - part - 1

---
+++ 08-webappserver-refactored/dataParser.js
@@ -0,0 +1,6 @@
+var url = require('url');
+
+module.exports = function(req){
+	req['urlObj'] = url.parse(req.url === '/' ? '/index.html' : req.url);
+}
+
---
+++ 08-webappserver-refactored/serveStatic.js
@@ -0,0 +1,21 @@
+var path = require('path'),
+	fs = require('fs');
+
+var staticExtns = ['.html', '.css', '.js', '.jpg', '.png', '.ico', '.xml', '.json', '.txt'];
+
+function isStatic(resource){
+	return staticExtns.indexOf(path.extname(resource)) >= 0;
+}
+
+module.exports = function(req, res){
+	if (isStatic(req.urlObj.pathname)){
+		var resourceName = path.join(__dirname, req.urlObj.pathname);
+		if (!fs.existsSync(resourceName)){
+			res.statusCode=404;
+			res.end();
+			return;
+		}
+		var stream = fs.createReadStream(resourceName);
+		stream.pipe(res);
+	}
+}
